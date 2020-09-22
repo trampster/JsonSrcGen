8c2faddd6f2fade36e5aa8dfbade73b8464c1c5a
Daniel Hughes <trampster@gmail.com>
2020-09-22 22:54:12 +1200

    Add CoreRT test project

---
+++ CoreRTTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using JsonSrcGen;
+
+namespace CoreRTTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var convert = new JsonSrcGenConvert();
+            var json = convert.ToJson(new SimpleClass()
+            {
+                Age = 24,
+                Height = 65.5f,
+                Name = "Bilbo Baggins"
+            });
+            Console.WriteLine(json);
+        }
+    }
+}
