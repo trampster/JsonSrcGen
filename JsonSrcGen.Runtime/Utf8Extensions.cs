ec793bd3024bb2ab1389bd2f42c5772db6928856
Daniel Hughes <trampster@gmail.com>
2020-11-28 20:58:38 +1300

    Add JsonSrcGen.Runtime project that will contain code needed at runtime.

---
+++ JsonSrcGen.Runtime/Utf8Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace JsonSrcGen.Runtime
+{
+    public static class Utf8Extensions
+    {
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int WriteUtf8(this byte[] utf8, int start, string value)
+        {
+            unsafe
+            {
+                int length = value.Length*2;
+                fixed (void* voidPtr = value)
+                fixed (byte* utf8Ptr = utf8)
+                {
+                    byte* spanPtr = (byte*)voidPtr;
+                    int utf8Index = start;
+                    for(int index = 0; index < length; index+=2, utf8Index++)
+                    {
+                        utf8Ptr[utf8Index] = spanPtr[index];
+                    }
+                    return start+value.Length;
+                }
+            }
+        }
+    }
+}
